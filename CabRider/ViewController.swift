8bcc13835a1037137103235f3c41f6c87561e057
Anurag <anurag.sharma1@grofers.com>
2018-02-22 13:39:50 +0530

    Initial Commit

---
+++ CabRider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CabRider
+//
+//  Created by Anurag on 22/02/18.
+//  Copyright © 2018 DrawnzerGames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
